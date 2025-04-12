4f2c4f4d4131fcf570ccd65baf2ae58f1f22611f
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-04-12 23:22:54 +0900

    feat: flashcard, cardのmigration, association, controller, router を記述 #14

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,6 @@
 class Card < ApplicationRecord
+  # Association
+  belongs_to :flashcard
+
+  # Validation
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_02_050858) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_11_011005) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,6 +30,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_02_050858) do
     t.string "source_video_timestamp"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "flashcard_id", null: false
+    t.index ["flashcard_id"], name: "index_cards_on_flashcard_id"
   end
 
   create_table "flashcards", force: :cascade do |t|
@@ -37,6 +39,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_02_050858) do
     t.boolean "shared"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
+    t.integer "input_target"
+    t.integer "output_target"
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_flashcards_on_user_id"
   end
 
   create_table "learning_histories", force: :cascade do |t|
@@ -71,4 +78,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_02_050858) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "cards", "flashcards"
+  add_foreign_key "flashcards", "users"
 end
