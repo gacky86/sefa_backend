7d14c91e52ab443ff25427fd1b8e6475bebda1d8
Ryuya Inagaki <77526127+gacky86@users.noreply.github.com>
2025-04-24 16:55:53 +0900

    feat: アカウント作成機能実装 #24

--- config/application.rb
+++ config/application.rb
@@ -45,5 +45,19 @@ module SefaBackend
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies # Required for all session management
     config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+
+
+    # config.middleware.use ActionDispatch::Flash
+    # config.middleware.insert_before 0, Rack::Cors do
+    #   allow do
+    #     # 今回はRailsのポートが3000番、Reactのポートが3001番にするので、Reactのリクエストを許可するためにlocalhost:3001を設定
+    #     origins 'localhost:3001'
+    #     resource '*',
+    #              :headers => :any,
+    #              # この一文で渡される、'access-token'、'uid'、'client'というheaders情報を用いてログイン状態を維持する。
+    #              :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+    #              :methods => [:get, :post, :options, :delete, :put]
+    #   end
+    # end
   end
 end
